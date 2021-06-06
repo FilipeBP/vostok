88d3b7c257ee891983ac8c581f57adb90e7ea142
FilipeBP <filipebritoperez@hotmail.com>
2021-06-06 19:49:07 -0300

    Add utils to the server

---
+++ server/utils.py
@@ -0,0 +1,28 @@
+from bson import ObjectId
+from pydantic.main import BaseModel
+
+# MongoDB needs an '_id' field, however pydantic doesn't allow variables starting with '_'
+class AliasObjectId(ObjectId):
+    
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    # Checks if the data received is valid
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid ObjectId')
+        return v
+
+    # Avoid an error when accessing the documentation
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+
+class MongoBaseModel(BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
