14d98bce410bbf64a8c7da211944433325b4f780
FilipeBP <filipebritoperez@hotmail.com>
2021-06-06 09:20:58 -0300

    Fill clients and product collections

--- server/database.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://filipeUser:<password>@cluster0.tfv4a.mongodb.net/test')
-db = client['mercosProject']
--- server/main.py
+++ server/main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
 from orders.routers import client, product
+from orders.database import db
+import os
+import json
 
 app = FastAPI()
 
 app.include_router(client.router)
 app.include_router(product.router)
+
+async def filling_collection(collection_name, file_name):
+    collections = await db.list_collections()
+    if len(list(filter(lambda x: x['name'] == collection_name, collections))) > 0:
+        return
+
+    file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', file_name))
+    with open(file) as f:
+        data = json.load(f)
+        await db[collection_name].insert_many(data)
+
+@app.on_event('startup')
+async def filling_collections():
+    await filling_collection('clients', 'clients.json')
+    await filling_collection('products', 'products.json')
+    
 
 @app.get('/')
 def index():
