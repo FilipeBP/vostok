ea70bc8cca14d5cada43e2beb6ee1533709383bb
FilipeBP <filipebritoperez@hotmail.com>
2021-06-06 19:46:51 -0300

    Change folders structure

--- server/main.py
+++ server/main.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI
-from orders.routers import client, product
-from orders.database import db
+from database import db
 import os
 import json
 
+from apps.clients.routers import router as client_router
+from apps.products.routers import router as product_router
+
 app = FastAPI()
 
-app.include_router(client.router)
-app.include_router(product.router)
+app.include_router(client_router)
+app.include_router(product_router)
 
 async def filling_collection(collection_name, file_name):
     collections = await db.list_collections()
-    if len(list(filter(lambda x: x['name'] == collection_name, collections))) > 0:
+    collections_filtered = filter(lambda x: x['name'] == collection_name, collections)
+    if len(list(collections_filtered)) > 0:
         return
 
     file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', file_name))
     with open(file) as f:
         data = json.load(f)
         await db[collection_name].insert_many(data)
+        print(f'Inserted data to {collection_name} with success')
 
 @app.on_event('startup')
 async def filling_collections():
